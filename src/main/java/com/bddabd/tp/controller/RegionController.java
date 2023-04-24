a24c923dfe7fe957e351e49b317bf28da9b08bc8
AugusR7 <augusramseyer197@gmail.com>
2023-04-23 23:21:25 -0300

    Ejercicio 2 99% resuelto

--- src/main/java/com/bddabd/tp/controller/RegionController.java
+++ src/main/java/com/bddabd/tp/controller/RegionController.java
@@ -1,20 +1,24 @@
 package com.bddabd.tp.controller;
 
 import com.bddabd.tp.dto.RegionDTO;
+import com.bddabd.tp.service.CammesaService;
 import com.bddabd.tp.service.RegionService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import com.bddabd.tp.service.CammesaService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class RegionController {
 
     private RegionService regionService;
+    private CammesaService cammesaService;
 
-    RegionController(RegionService regionService) {
+    RegionController(RegionService regionService, CammesaService cammesaService) {
         this.regionService = regionService;
+        this.cammesaService = cammesaService;
     }
 
     @GetMapping("/getRegionsByName")
@@ -38,8 +42,15 @@ public class RegionController {
 
     @GetMapping("/demandaFeriadoMasCercano")
     public String demandaFeriadoMasCercano(@RequestParam(value = "fecha") String fecha) {
-        List<HashMap> countryDemand = regionService.getCountryDemandOnDate(fecha, 1002);
-        System.out.println(countryDemand);
-        return "Hola";
+        String feriadoMasCercanoALaFecha = cammesaService.feriadoMasCercano(fecha);
+        System.out.println(feriadoMasCercanoALaFecha);
+        List<HashMap> countryDemand = regionService.getCountryDemandOnDate(feriadoMasCercanoALaFecha, 1002);
+        Integer demanda = 0;
+        for( int i = 0; i < countryDemand.size(); i++){
+            demanda += (Integer) countryDemand.get(i).get("dem");
+        }
+        String mensaje = "La demanda promedio del país en lá fecha \"" + feriadoMasCercanoALaFecha +
+                "\" fue de: " + demanda/countryDemand.size() + " MW";
+        return mensaje;
     }
 }
--- src/main/java/com/example/demo/exceptions/DateException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-public class DateException extends RuntimeException {
-    public DateException(String message) {
-        super(message);
-    }
-
-}
