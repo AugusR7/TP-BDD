a7c04bb37cef08afc720b9a0e5e69debc94ba115
gonza0086 <hgonzalo2000@gmail.com>
2023-05-19 16:36:29 -0300

    batch in background

--- src/main/java/com/bddabd/tp/controller/JobController.java
+++ src/main/java/com/bddabd/tp/controller/JobController.java
@@ -5,10 +5,13 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,17 +23,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobController {
 
     @Autowired private Job job;
-    @Autowired  private JobLauncher jobLauncher;
+    @Autowired  private JobRepository jobRepository;
+
+    public JobLauncher jobLauncher() throws Exception {
+        TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
+    }
 
 
     @PostMapping("/demandaYTemperaturaMensual")
     public String demandaYTemperaturaMensual(@RequestParam(value = "region") Integer region, @RequestParam("fecha") String fecha)
-            throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+            throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("fecha", fecha)
                 .addLong("region", region.longValue())
                 .toJobParameters();
-        jobLauncher.run(job, jobParameters);
+        jobLauncher().run(job, jobParameters);
         return "Batch job has been invoked";
     }
 
